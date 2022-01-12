77f73f89795f204094f59d4178bb6be99eaff41f
Prakhar Ojha <prakhar@getStan.app>
2022-01-12 22:01:22 +0530

    Game added

---
+++ day3/game/index.js
@@ -0,0 +1,7 @@
+const ourChoice = documented.getElementByClass('ourChoice');
+const botChoice = document.getElementByClass('botChoice');
+
+const choseStone = (e) =>{
+e.preventDefault();
+console.log(e.target);
+}
